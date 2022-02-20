11101f2fb0c84957aa8c1f157a674f9a21daa3ff
Shraddha-V22 <shraddhav1935@gmail.com>
2022-02-20 23:20:14 +0530

    update app.js
    added server URL of the minion language, variable to take input from the user, to process it and then to show output to the user.

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function constructURl(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occurred", error)
+};
+
 function clickHandler() {
-    outputDiv.innerText = "translation: " + textInput.value
+    var inputText = textInput.value
+
+    fetch(constructURl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedValue = json.contents.translated
+        outputDiv.innerText = translatedValue})
+    .catch(errorHandler);
 };
 
 btnTranslate.addEventListener("click", clickHandler);
